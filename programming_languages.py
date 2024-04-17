bb0d9194d98ae905fd98abea9aa1ae6545df17dd
Kyle Lawrence <35710698+kylegwlawrence@users.noreply.github.com>
2024-04-17 15:54:10 -0600

    Add a function to get data from a specific Wikipedia page and write to csv”

--- programming_languages.py
+++ programming_languages.py
@@ -16,34 +16,40 @@ def table_from_html(page_name, table_index):
         df = pd.read_html(html)[0]
     return df
 
+def chronological_programming_langs(save_path):
+    """grabs data on programming languages including
+    language name, year created, developers (ie the
+    creators) and predecessor languages if applicable"""
+    
+    start = time.time()
 
-start = time.time()
+    page_name = 'timeline of programming languages'
+    first_table_index = 4
+    last_table_index = 12
 
-page_name = 'timeline of programming language'
-first_table_index = 4
-last_table_index = 12
+    frames = []
+    for table_index in range(first_table_index, last_table_index+1):
+        df = table_from_html(page_name, table_index)
+        frames.append(df)
+    df = pd.concat(frames)
 
-frames = []
-for table_index in range(first_table_index, last_table_index+1):
-    df = table_from_html(page_name, table_index)
-    frames.append(df)
-df = pd.concat(frames)
+    df.rename(columns={
+        "Year": "year_created"
+        ,"Name":"language_name"
+        ,"Chief developer, company":"developer"
+        ,"Predecessor(s)":"predecessor"
+        }, inplace=True)
+        
+    df['year_created'] = df['year_created'].str.replace('?', '')
+    df['year_created'] = df['year_created'].str[:4]
+    df['predecessor'] = df['predecessor'].str.replace('"', '')
+    df['developer'] = df['developer'].str.replace('"', '')
+    df['language_name'] = df['language_name'].str.replace("′′",'')
 
-df.rename(columns={
-    "Year": "year_created"
-    ,"Name":"language_name"
-    ,"Chief developer, company":"developer"
-    ,"Predecessor(s)":"predecessor"
-    }, inplace=True)
+    df.to_csv(save_path, index=False)
 
-#df['year_created'] = df['year_created'].replace(['1964?'], '1964')
-df['year_created'] = df['year_created'].str.replace('?', '')
-df['year_created'] = df['year_created'].str[:4]
-df['predecessor'] = df['predecessor'].str.replace('"', '')
-df['developer'] = df['developer'].str.replace('"', '')
-df['language_name'] = df['language_name'].str.replace("′′",'')
-
-df.to_csv('programming_languages.csv', index=False)
-
-end = time.time()
-print(f"Elapsed time: {round((end - start),2)} seconds")
+    end = time.time()
+    print(f"Elapsed time: {round((end - start),2)} seconds")
+    
+if __name__ == "__main__":
+    chronological_programming_langs("programming_languages.csv")
